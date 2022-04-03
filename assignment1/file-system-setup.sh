bc542a082c3798c5021e54b6de0d61f273450476
Matt Romanes <48772682+romanesmatt@users.noreply.github.com>
2022-04-04 11:55:08 +1200

    Add files via upload

---
+++ assignment1/file-system-setup.sh
@@ -0,0 +1,18 @@
+# Matt Romanes
+# romanematt
+# 300492211
+
+# Question 2
+
+!/bin/bash
+
+# Creating the directory WellingtonClinic inside the /opt/directory
+
+mkdir WellingtonClinic
+    cd WellingtonClinic 
+    mkdir Scripts Staff Patients
+        cd Staff
+        mkdir Administrators Nurses Receptionists Doctors
+        
+
+
---
+++ assignment1/staff-create.sh
@@ -0,0 +1,12 @@
+# Matt Romanes
+# romanematt
+# 300492211
+
+# Question 3
+
+!/bin/bash
+
+# Creating directories for each staff member
+
+
+
