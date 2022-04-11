91c57fb57b8bff6a7d8c467aad46681dd9dcb321
Matt <mattromanes@gmail.com>
2022-04-11 21:17:54 +1200

    Wipe Q3 for re-attempt

--- assignment1/acls.sh
+++ assignment1/acls.sh
@@ -6,35 +6,3 @@
 
 #!/bin/bash
 
-
-cd ../../
-
-sudo setfacl -Rm g:Administrator:rwx WellingtonClinic
-sudo setfacl -m o:r-x WellingtonClinic
-
-cd ../../
-cd WellingtonClinic
-
-sudo setfacl -Rm o:r-x Staff
-sudo setfacl -Rm o:r-x Staff
-sudo setfacl -Rm o:r-x Staff
-sudo setfacl -Rm o:r-x Staff
-
-cd ../../
-cd WellingtonClinic
-cd Scripts
-
-# GET BACK TO THIS LATER
-
-sudo setfacl -m g:Administrator:rwx audit.sh
-sudo setfacl -m g:Receptionists:r-x searchdoctor.sh
-sudo setfacl -m g:Receptionists:r-x register-patient.sh
-sudo setfacl -m g:Doctors:r-x visit.sh
-sudo setfacl -m g:Doctors:r-x register-patient.sh
-sudo setfacl -m g:Nurses:r-x register-patient.sh
-
-cd ../../
-cd WellingtonClinic
-#Double checking that only the Receptionist group is the owner of the Patients group
-sudo chgrp Receptionists Patients
-sudo setfacl -Rm g:Administrator:rwx Patients
